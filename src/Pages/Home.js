8eb96850fcef07e547ce56151e39e456d5910152
MoApplesNOranges <billmo0120@gmail.com>
2025-01-27 17:45:50 -0500

    deploy-2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import DSC_1200 from "../images/DSC_1200.jpg";
-import IMG_1675 from "../images/IMG_1675.jpg";
-import IMG_1715 from "../images/IMG_1715.jpg";
+// import DSC_1200 from "../images/DSC_1200.jpg";
+// import IMG_1675 from "../images/IMG_1675.jpg";
+// import IMG_1715 from "../images/IMG_1715.jpg";
 import "./Home.css";
 import { NavLink } from "react-router-dom";
 
@@ -14,17 +14,17 @@ const Home = () => {
       <div className="grid">
         <div className="one">
           <NavLink to="/photography">
-            <img src={DSC_1200} alt="heron photo" />
+            <img src="https://i.imgur.com/b2LOHC3.jpeg" alt="heron photo" />
           </NavLink>
         </div>
         <div className="two">
           <NavLink to="/paintings">
-            <img src={IMG_1675} alt="elephant photo" />
+            <img src="https://i.imgur.com/fTZNkXy.jpeg" alt="elephant photo" />
           </NavLink>
         </div>
         <div className="three">
           <NavLink to="/chineseart">
-            <img src={IMG_1715} alt="chinese photo" />
+            <img src="https://i.imgur.com/0Yvgon5.jpeg" alt="chinese photo" />
           </NavLink>
         </div>
       </div>
