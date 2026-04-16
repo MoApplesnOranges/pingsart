ed2b5272060d2ac1e2378957c6e7592d429760df
MoApplesNOranges <billmo0120@gmail.com>
2026-04-16 10:16:43 -0400

    fix error

--- src/Pages/data/paintings.js
+++ src/Pages/data/paintings.js
@@ -1,11 +1,11 @@
 import elephant from "../images/elephant.jpg";
 import Red_Cardinal from "../images/Red_Cardinal.jpeg";
-import lover_swans from "../images/lover_swans.jpeg";
+import Lover_Swans from "../images/lover_swans.jpeg";
 
 const paintings = [
   { id: "1", src: elephant, title: "Elephant by the River" },
   { id: "2", src: Red_Cardinal, title: "Red Cardinal eating Cherries" },
-  { id: "3", src: lover_swans, title: "Lover Swans" },
+  { id: "3", src: Lover_Swans, title: "Lover Swans" },
 ];
 
 export default paintings;
